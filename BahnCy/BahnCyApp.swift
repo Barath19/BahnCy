f54cfa002e7f04d2cfc10a10360ccfa6e8b4b9be
Barath19 <30743936+Barath19@users.noreply.github.com>
2025-08-20 00:35:58 +0200

    Initial Commit

---
+++ BahnCy/BahnCyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BahnCyApp.swift
+//  BahnCy
+//
+//  Created by Bharath Kumar on 20.08.25.
+//
+
+import SwiftUI
+
+@main
+struct BahnCyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BahnCy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BahnCy
+//
+//  Created by Bharath Kumar on 20.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
